5c9f072e69e6e44ecb51e954b6f630350a79fad6
Milene Resnik <141060425+m1lene@users.noreply.github.com>
2024-04-12 10:35:24 -0300

    Update client.py

--- client.py
+++ client.py
@@ -1,13 +1,22 @@
-from socket import * 
-
 def main():
-       clientSocket = socket(AF_INET, SOCK_STREAM)
-       clientSocket.connect(('127.0.0.1)', 2222))
-       message = input("mensagem:")
-       clientSocket.send(message.encode())
-       response, addr = clientSocket.recv(2048)
-       print(response.decode())
-       clientSocket.close()
-
-if __name__ == '__main__':
-    main()
+    clientSocket = socket(AF_INET, SOCK_STREAM)
+    clientSocket.connect(('127.0.0.1', 2222))
+    
+    while True:
+        response = clientSocket.recv(2048).decode()
+        if not response:
+            print("Atendente encerrou a conexão.")
+            break
+        
+        print("Atendente:", response)
+        if response.lower() == "tchau":
+            print("Atendente encerrou a conexão.")
+            break
+        
+        message = input("Cliente: ")
+        clientSocket.send(message.encode())
+        if message.lower() == "tchau":
+            print("Cliente encerrou a conexão.")
+            break
+    
+    clientSocket.close()

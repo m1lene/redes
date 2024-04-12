5b1e823b1cbd975465d37e472064f34102688a86
Milene Resnik <141060425+m1lene@users.noreply.github.com>
2024-04-12 11:32:40 -0300

    Update server.py

--- server.py
+++ server.py
@@ -8,22 +8,17 @@ def main():
     while True:
         connectionSocket, addr = serverSocket.accept()
         print("Conexão estabelecida com", addr)
-        connectionSocket.send("Bem-vindo ao atendimento de call center! Como posso ajudá-lo?".encode())
-        
+        connectionSocket.send("Bem-vindo ao atendimento de call center. Como posso ajudar?\nDigite 'Tchau' para encerrar a conversa.\n".encode())
         while True:
             message = connectionSocket.recv(2048).decode()
             print("Cliente:", message)
             if message.lower() == "tchau":
-                print("Cliente encerrou a conexão.")
+                print("Cliente encerrou a conversa.")
                 break
-            
             response = input("Atendente: ")
             connectionSocket.send(response.encode())
-            if response.lower() == "tchau":
-                print("Atendente encerrou a conexão.")
-                break
-
         connectionSocket.close()
 
 if __name__ == '__main__':
     main()
+
